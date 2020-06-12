5c27db979756482a12f1381b91506b976e471678
St. Hatijah H.Ilyas <57305410+13020180061@users.noreply.github.com>
2020-06-13 00:04:44 +0800

    Add files via upload

---
+++ mahasiswa.sql
@@ -0,0 +1,6 @@
+create database Mahasiswaaa;
+use Mahasiswaaa;
+
+create table table_mahasiswaa ( `Stambuk` varchar(5) primary key,
+`Nama` varchar(30),
+`Jurusan` varchar(15), `Email` varchar(30));
